15d5ec1fe2b2254df41d3eabf0ae545df2a3cad8
Josef Möllers <20593010+jmoellers@users.noreply.github.com>
2019-11-08 11:45:48 +0100

    Create Read.c

---
+++ Read.c
@@ -0,0 +1,47 @@
+# include       <stdio.h>
+# include       <unistd.h>
+# include       <inttypes.h>
+# include       "MFRC522.h"
+    
+int 
+main(int argc, char *argv[])
+{   
+    uint8_t mode, bits; 
+    int status;
+    unsigned char backBits;
+    unsigned char *uid;
+    
+    MFRC522_Init(0);
+    
+    
+    while ((status = MFRC522_Request(PICC_REQIDL, &backBits)) != MI_OK)
+        usleep(500);
+    // status = MFRC522_Request(PICC_REQIDL, &backBits);
+    
+    // If a card is found
+    if (status == MI_OK)
+        printf("Card detected\n");
+    
+    status = MFRC522_Anticoll(&uid);
+    
+    if (status == MI_OK)
+    {
+        unsigned char key[] = { 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF };        // This is the default key for authentication
+        // print UID
+        printf("%02x %02x %02x %02x\n", uid[0], uid[1], uid[2], uid[3]);
+    
+        // Select the scanned tag
+        MFRC522_SelectTag(uid);
+
+        status = MFRC522_Auth(PICC_AUTHENT1A, 8, key, sizeof(key), uid);
+
+        // Check if authenticated
+        if (status == MI_OK) 
+        {
+            MFRC522_Read(8);
+            MFRC522_StopCrypto1();
+        }
+        else
+            fprintf(stderr, "Authentication error");
+    }
+}
